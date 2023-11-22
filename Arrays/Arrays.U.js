60f42bb5136e5475f573af087c41db0e9ff28848
Rosamata22 <rosamatapavia@gmail.com>
2023-11-22 16:24:45 +0100

    Improvement ex.4

--- Arrays/Arrays.U.js
+++ Arrays/Arrays.U.js
@@ -14,8 +14,9 @@ schön formatiert aus.
      brand: "Seat",
      Motornummer: 897863805742,
      configuration:["Türe", "asset1", "asset2", "asset3"],
-
  }
+ console.log(`Hier steht ${car.configuration}...`)
+
  /* Nutze die selbe Tabelle als zweidimensionales Array, aber lass den Nutzer eine
  eine Zeilennummer und den Namen einer Spalte angeben.Nutze
  zur Zuordnung des Spaltensnamens zu einer Spaltennummer ein Objekt. Gib den gewünschten Wert aus.
@@ -30,14 +31,43 @@ const Tabelle1 = [
 ]
 
 console.log(Tabelle1)
+//********************************IMPORTANTE********************************//
+/*Nutze die selbe Tabelle als zweidimensionales Array, aber lass den Nutzer
+eine Zeilennummer und den Namen einer Spalte angeben. Nutze zur
+Zuordnung des Spaltennamens zu einer Spaltennummer ein Objekt. Gib den
+gewünschten Wert aus.*/
+const readline = require('readline');
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+const prompt = (query) => new Promise((resolve) => rl.question(query, resolve));
+rl.on('close', () => process.exit(0));
+async function execute() {
+// Euer Code kommt hier hinein
+    const line = parseFloat(await prompt("Please enter the desidered line number: "));
+    const column = await prompt("Please enter the column: ")
 
-const Tabelle2 = {Vorname:0, Nachname:1, Alter:2};
-const Tabelle2a = [
+    const columnNames = {Vorname:0, Nachname:1, Alter:2};
+    const Tabelle2a = [
     ["Hans", "Müller", 22],
     ["Georg", "Huber", 37],
     ["Fritz", "Mayr", 19]
-]
-console.log(Tabelle2)
+    ]
+const spaltenIndex = columnNames[column];
+console.log(Tabelle2a[line][spaltenIndex]);
+
+
+    /* otra manera con Arrays, sin objetos*/
+
+    const lineArr = parseFloat(await prompt("Please enter the desidered line number: "));
+    const columnArr = await prompt("Please enter the column: ")
+const columnNamesArr = ["Vorname", "Nachname","Alter"];
+    const spaltenIndexArr = columnNamesArr.indexOf(columnArr)
+    console.log(Tabelle2a[lineArr][spaltenIndexArr]);
+
+
+
+/*5. Lege die selbe Tabelle als Array von Objekten an, wobei die Spaltennamen
+die Schlüssel in den Objekten darstellen. Lass den Nutzer eine Zeilennummer
+und den Namen einer Spalte angeben, gib den gewünschten Wert aus.*/
 
 const Tabelle3 = [
     {Vorname:"Hans", Nachname:"Müller", Alter:22},
@@ -45,3 +75,7 @@ const Tabelle3 = [
     {Vorname:"Fritz", Nachname:"Mayr", Alter: 19}]
 console.log(Tabelle3[0].Vorname)
 
+
+
+}
+execute().finally(() => rl.close());
