adeb22496883ce1522211668766b1e48fff23882
Rosamata22 <rosamatapavia@gmail.com>
2023-12-06 11:23:52 +0100

    .jason Package richtig hinzugefügt ;)

--- Filesystem/Filesystem_Callback_API.js
+++ Filesystem/Filesystem_Callback_API.js
@@ -10,9 +10,31 @@ werden, sonst wird das Programm unter Umständen zu früh beendet. Verändert si
 ihr das Programm beenden möchtet (nach dem Schreiben der Datei (im callback)).
  */
 
-import { readFile } from 'node:fs';
-readFile('./data.json', 'utf8', async (err, data) => {
-});
-console.log('File read', err, data);
-// Zum Beenden des Programs `rl.close()` aufrufen
+import {readFile} from 'node:fs';
+readFile('', 'utf8', async (err, data) => {
+if (err) {
+    console.error('Error reading file');
+}else{
+console.log('File read', err, data);}});
+
+const prod1 = {Produktnummer:1, Bezeichnung:"Banane",preis:1.79};
+const prod2 = {Produktnummer:11, Bezeichnung:"Bio-Banane",preis:2.30};
+const prod3 = {Produktnummer:2, Bezeichnung:"Boccoli",preis:2};
+const prod4 = {Produktnummer:21, Bezeichnung:"Bio-Broccoli",preis:2.5};
+const prod5 = {Produktnummer:3, Bezeichnung:"Gurken",preis:0.79};
+const prod6 = {Produktnummer:31, Bezeichnung:"Bio-Gurken",preis:1.25};
+const prod7 = {Produktnummer:4, Bezeichnung:"Roma Tomaten",preis:1.85};
+const prod8 = {Produktnummer:41, Bezeichnung:"Bio-Roma Tomaten",preis:2.5};
+const prod9 = {Produktnummer:5, Bezeichnung:"San Marzano Tomaten",preis:2.5};
+const prod10 = {Produktnummer:6, Bezeichnung:"Cherry Tomaten",preis:15};
+
+writeFile('C:\\Users\\rosam\\Desktop\\Codersbay\\Web\\Javascript\\JavascriptExercises\\Filesystem\\FileSystem_Uebung.json', prod1,prod2,prod3, prod4,prod5,prod6,prod7,prod8,prod9,prod10, 'utf8', (err) => {
+// auf Fehler prüfen
+    if (Object.preis===0) {
+        console.error('Error writing file');
+    } else {
+        console.log('File saved successfully!');
+    }
+
+
 execute().catch((err) => { console.error(err); });// .finally(() => rl.close());
